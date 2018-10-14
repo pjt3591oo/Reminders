08f323b53a6c7b8dc6da83283b232c172c1c553a
박정태 <pjt@bagjeongtaeui-iMac.local>
2018-10-14 15:57:55 +0900

    GET /reminder_list/:list_id/item를 추가한다

--- routes/API_V_1/reminderItem.js
+++ routes/API_V_1/reminderItem.js
@@ -33,4 +33,27 @@ router.post('/:listId', async (req, res) => {
     }
 })
 
+router.get('/:listId', async (req, res) => {
+    let { listId } = req.params
+
+    if ( !listId ) {
+        return res.status(404).json({msg: `parameter가 충분하지 않습니다.(listId)`})
+    }
+
+    try{
+        let reminderListItems = await reminder.findOne({
+            where: {id: listId},
+            include: [
+                {
+                    model: reminderListItem
+                }
+            ]
+        })
+        console.log(reminderListItems)
+        return res.status(200).json(reminderListItems)
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+})
+
 module.exports = router;
