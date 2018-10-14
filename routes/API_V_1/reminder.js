715e4257b1f4e021d84e8dafc90b1a28a826d6ac
박정태 <pjt@bagjeongtaeui-iMac.local>
2018-10-14 15:21:27 +0900

    GET /reminder_list를 추가한다

--- routes/API_V_1/reminder.js
+++ routes/API_V_1/reminder.js
@@ -10,9 +10,21 @@ router.post('/', async (req, res) => {
         let createdReminder = await reminder.create({
             name: name
         })
-        
+
         return res.status(201).json(createdReminder)
-    } catch(err) {
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+})
+
+router.get('/', async (req, res) => {
+    try{
+        let reminders = await reminder.findAll({
+            order: [['createdAt', 'DESC']]
+        })
+
+        return res.status(200).json({lists: reminders})
+    } catch (err) {
         return res.status(500).json(err)
     }
 })
